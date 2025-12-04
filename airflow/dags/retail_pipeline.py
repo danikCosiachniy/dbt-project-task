e4854e81d9ad58112cf4523d10cc19492c243fbc
danikCosiachniy <danilashumkevich@gmail.com>
2025-12-04 17:37:16 +0300

    make the logging of tasks with loguru

--- airflow/dags/retail_pipeline.py
+++ airflow/dags/retail_pipeline.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 from airflow import DAG
 from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig
+from utils.dbt_logger import log_success_callback, log_failure_callback, log_start_callback
 
 DBT_PROJECT_PATH = "/opt/airflow/dbt_project"
 
@@ -11,11 +12,19 @@ profile_config = ProfileConfig(
     profiles_yml_filepath=f"{DBT_PROJECT_PATH}/profiles.yml"
 )
 
+default_args = {
+    "owner": "airflow",
+    "on_failure_callback": log_failure_callback,
+    "on_success_callback": log_success_callback,
+    "on_execute_callback": log_start_callback
+}
+
 with DAG(
     dag_id="retail_vault_dag",
     start_date=datetime(2023, 1, 1),
     schedule_interval="@daily",
     catchup=False,
+    default_args=default_args,
     tags=["dbt", "retail", "duckdb"],
 ) as dag:
 
