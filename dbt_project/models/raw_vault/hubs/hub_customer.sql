d9c45e3c9f3bf210607aa76e386fab7ca661d0af
danikCosiachniy <danilashumkevich@gmail.com>
2025-12-05 17:08:29 +0300

    make sql linter
    Forget it yesterday, yeah it's my mistake)

--- dbt_project/models/raw_vault/hubs/hub_customer.sql
+++ dbt_project/models/raw_vault/hubs/hub_customer.sql
@@ -1,17 +1,17 @@
 {{ config(materialized='incremental') }}
 
 SELECT DISTINCT
-    source.order_customer_link_hk,
-    source.order_hk,
-    source.customer_hk,
-    CURRENT_TIMESTAMP as load_date,
-    source.record_source
-FROM {{ ref('stg_orders') }} as source
+    source.order_customer_link_hk
+    , source.order_hk
+    , source.customer_hk
+    , source.record_source
+    , CURRENT_TIMESTAMP AS load_date
+FROM {{ ref('stg_orders') }} AS source
 
 {% if is_incremental() %}
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM {{ this }} as target
-    WHERE target.order_customer_link_hk = source.order_customer_link_hk
-)
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM {{ this }} AS target
+        WHERE target.order_customer_link_hk = source.order_customer_link_hk
+    )
 {% endif %}
--- dbt_project/models/staging/stg_orders.sql
+++ dbt_project/models/staging/stg_orders.sql
@@ -2,20 +2,20 @@
 
 WITH raw_data AS (
     SELECT * FROM {{ ref('raw_orders') }}
-),
+)
 
-hashed_data AS (
+, hashed_data AS (
     SELECT
-        order_id,
-        customer_id,
-        SHA2(CAST(order_id AS VARCHAR), 256) as order_hk,
-        SHA2(CAST(customer_id AS VARCHAR), 256) as customer_hk,
-        SHA2(CAST(order_id AS VARCHAR) || CAST(customer_id AS VARCHAR), 256) as order_customer_link_hk,
-        SHA2(CAST(order_date AS VARCHAR) || CAST(status AS VARCHAR), 256) as order_hashdiff,
-        order_date,
-        status,
-        'raw_orders_csv' as record_source,
-        CURRENT_TIMESTAMP as load_date
+        order_id
+        , customer_id
+        , order_date
+        , status
+        , 'raw_orders_csv' AS record_source
+        , SHA2(CAST(order_id AS VARCHAR), 256) AS order_hk
+        , SHA2(CAST(customer_id AS VARCHAR), 256) AS customer_hk
+        , SHA2(CAST(order_id AS VARCHAR) || CAST(customer_id AS VARCHAR), 256) AS order_customer_link_hk
+        , SHA2(CAST(order_date AS VARCHAR) || CAST(status AS VARCHAR), 256) AS order_hashdiff
+        , CURRENT_TIMESTAMP AS load_date
     FROM raw_data
 )
 
