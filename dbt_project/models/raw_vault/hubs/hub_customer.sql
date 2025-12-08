739a0b42214a25db5acdfcbb0fc30b97a8f8e8cf
danikCosiachniy <danilashumkevich@gmail.com>
2025-12-08 13:13:50 +0300

    solve bug by ci_cd

--- dbt_project/models/raw_vault/hubs/hub_customer.sql
+++ dbt_project/models/raw_vault/hubs/hub_customer.sql
@@ -1,17 +1,18 @@
 {{ config(materialized='incremental') }}
 
 SELECT DISTINCT
-    source.order_customer_link_hk
-    , source.order_hk
-    , source.customer_hk
+    source.customer_hk
+    , source.customer_id
     , source.record_source
     , CURRENT_TIMESTAMP AS load_date
 FROM {{ ref('stg_orders') }} AS source
+WHERE
+    source.customer_hk IS NOT NULL
 
-{% if is_incremental() %}
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM {{ this }} AS target
-        WHERE target.order_customer_link_hk = source.order_customer_link_hk
-    )
-{% endif %}
+    {% if is_incremental() %}
+        AND NOT EXISTS (
+            SELECT 1
+            FROM {{ this }} AS target
+            WHERE target.customer_hk = source.customer_hk
+        )
+    {% endif %}
