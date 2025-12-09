8becfe33f8909b2f6cd010585b85604d3840fec9
danikCosiachniy <danilashumkevich@gmail.com>
2025-12-09 15:49:02 +0300

    make a chance of switching output db( snowflake or duckdb) and automatically load creeds

--- airflow/dags/utils/constants.py
+++ airflow/dags/utils/constants.py
@@ -2,3 +2,5 @@
 DBT_PROJECT_PATH: str = '/opt/airflow/dbt_project'
 # Path to the central dbt/Airflow log file inside the container
 LOG_FILE_PATH: str = '/opt/airflow/logs/dbt_pipeline.log'
+# The name of variable in Airflow with your creeds
+VAR_NAME: str = 'dbt_snowflake_secrets'
--- dbt_project/models/staging/stg_orders.sql
+++ dbt_project/models/staging/stg_orders.sql
@@ -11,10 +11,10 @@ WITH raw_data AS (
         , order_date
         , status
         , 'raw_orders_csv' AS record_source
-        , SHA2(CAST(order_id AS VARCHAR), 256) AS order_hk
-        , SHA2(CAST(customer_id AS VARCHAR), 256) AS customer_hk
-        , SHA2(CAST(order_id AS VARCHAR) || CAST(customer_id AS VARCHAR), 256) AS order_customer_link_hk
-        , SHA2(CAST(order_date AS VARCHAR) || CAST(status AS VARCHAR), 256) AS order_hashdiff
+        , {{ hash_sha256("order_id") }} AS order_hk
+        , {{ hash_sha256("customer_id") }} AS customer_hk
+        , {{ hash_sha256("order_id || customer_id") }} AS order_customer_link_hk
+        , {{ hash_sha256("order_date || status") }} AS order_hashdiff
         , CURRENT_TIMESTAMP AS load_date
     FROM raw_data
 )
